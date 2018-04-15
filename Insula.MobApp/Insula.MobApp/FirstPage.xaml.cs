5c3d8a59c87d5b76204e2633923e7e43bc44aaa9
Chusovliankin Aleksei <lixich@mail.ru>
2018-04-15 16:24:51 +0500

    first commit

---
+++ Insula.MobApp/Insula.MobApp/FirstPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Insula.MobApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FirstPage : ContentPage
+	{
+		public FirstPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Click_Authorization(object sender, EventArgs e)
+        {
+            if (Entry_AuthorizationLogin.Text == Entry_AuthorizationPassword.Text)
+                Label_Authorization.Text = "Successful Authorization";
+            else
+                Label_Authorization.Text = "Login or password failed";
+        }
+    }
+}
