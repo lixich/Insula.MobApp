14c24bf7147b4f51a8e5e450d81bd4ac833ab367
Aleksei Chusovliankin <lixich@mail.ru>
2018-05-09 17:33:48 +0500

    fix url

--- Insula.MobApp/Insula.MobApp/Models/Constants.cs
+++ Insula.MobApp/Insula.MobApp/Models/Constants.cs
@@ -17,7 +17,7 @@ namespace Insula.MobApp.Models
 
         //-----Url-----
         //public static string Url = "http://192.168.0.102:5000/";
-        public static string Url = "https://08890fe8.ngrok.io/";
+        public static string Url = "https://24f0b404.ngrok.io/";
         public static string UserUrl = Url + "user/";
         public static string DiaryUrl = Url + "dose/";
         public static string ForecastUrl = Url + "forecast/";
